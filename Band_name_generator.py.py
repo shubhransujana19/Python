9f6a97fa0681877e497181b6a7cbd2c7c20974c5
shubhransujana19 <91710462+shubhransujana19@users.noreply.github.com>
2021-10-07 01:36:58 +0530

    Add files via upload

---
+++ Band_name_generator.py.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 17:45:28 2021
+
+@author: shubhransu
+"""
+print("Welcome to the band name generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name\n")
+print("Your band name could be " + city_name +" "+ pet_name)
+
+      
+
