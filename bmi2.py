e00a26d9a832e7d13ce49f0a88617693254ff5a1
shubhransujana19 <91710462+shubhransujana19@users.noreply.github.com>
2021-10-07 00:38:32 +0530

    Add files via upload

---
+++ bmi2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  1 15:45:02 2021
+
+@author: shubhransu
+"""
+weight = float(input("What is your weight? "))
+height = float(input("What is your height?"))
+bmi = 
+if weight<18.5:
+    print("they are underweight")
+elif 18.5<weight<25 
+    print("")
+
